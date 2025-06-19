ebf15b4a749203623ccde4495ec6faf09182d891
Zobor34 <zobor77@gmail.com>
2025-06-19 20:48:01 +0200

    Update fut_api.py

--- futapi/fut_api.py
+++ futapi/fut_api.py
@@ -1,17 +1,20 @@
 import requests
 
 class FutApiClient:
-   def get_players(self, limit=50):
-    url = f"https://www.futbin.org/api/players?platform={self.platform}&limit={limit}"
-    headers = {
-        "User-Agent": "Mozilla/5.0"
-    }
+    def __init__(self, platform="ps"):
+        self.platform = platform.lower()
+        self.base_url = "https://www.futbin.org/api"
 
-    response = requests.get(url, headers=headers)
-    if response.status_code == 200:
-        data = response.json()
-        return data if isinstance(data, list) else data.get("players", [])
-    else:
-        print(f"❌ Erreur API FUTBIN: {response.status_code}")
-        return []
+    def get_players(self, limit=50):
+        url = f"https://www.futbin.org/api/players?platform={self.platform}&limit={limit}"
+        headers = {
+            "User-Agent": "Mozilla/5.0"
+        }
 
+        response = requests.get(url, headers=headers)
+        if response.status_code == 200:
+            data = response.json()
+            return data if isinstance(data, list) else data.get("players", [])
+        else:
+            print(f"❌ Erreur API FUTBIN: {response.status_code}")
+            return []
